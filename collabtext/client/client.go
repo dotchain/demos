08d08322735bee39c71c1d02f7ce34f61eb7fb21
Ramesh VK <ramesh.vyaghrapuri@gmail.com>
2018-10-10 15:21:57 +0200

    Update client to use cache

--- collabtext/client/client.go
+++ collabtext/client/client.go
@@ -14,6 +14,7 @@ import (
 	"github.com/dotchain/dot/x/nw"
 	"log"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -28,7 +29,7 @@ func main() {
 	client := &nw.Client{URL: *argURL}
 	defer client.Close()
 
-	tx := ops.Transformed(client)
+	tx := ops.TransformedWithCache(client, &sync.Map{})
 	stream := streams.New()
 
 	sync := ops.NewSync(tx, -1, stream, idgen.New)
